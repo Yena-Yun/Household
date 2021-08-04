4bd3a25382d515ad1f7763c3c7b832e3bc74a254
Yena-Yun <cheryleduart@gmail.com>
2021-08-04 10:32:27 +0900

    초기 세팅

---
+++ household/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+function App() {
+  // const [name, setName] = useState("yena");
+  
+  // const callback = () => {
+  // console.log(name);
+  //   setName("linlin");
+	// console.log(name);
+  // }
+  
+  // callback();
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
